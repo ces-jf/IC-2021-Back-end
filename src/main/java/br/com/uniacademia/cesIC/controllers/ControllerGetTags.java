fa61906550d60ee2b48cc286e6cb314336bd23e4
ClaudiodeOliveira <claudiojf217@outlook.com>
2021-08-19 17:23:07 -0300

    commit10

--- src/main/java/br/com/uniacademia/cesIC/controllers/ControllerGetTags.java
+++ src/main/java/br/com/uniacademia/cesIC/controllers/ControllerGetTags.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -37,6 +38,7 @@ public class ControllerGetTags {
 		return ResponseEntity.ok(tagsFDTOs);
 	}
 
+	@GetMapping
 	public ResponseEntity<Response<List<GetTags>>> findAll() {
 		log.info("Start - ControllerGetTags.findAll");
 		Response<List<GetTags>> response = new Response<>();
